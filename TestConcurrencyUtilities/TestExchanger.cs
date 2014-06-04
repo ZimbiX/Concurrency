c9189f7e20dea1b825a2012c03a5ef787ed5cf83
Brendan Weibrecht <zimbico@gmail.com>
2014-06-04 17:00:08 +1000

    Exchanger: Update to reflect rename from Rendezvous

--- TestConcurrencyUtilities/TestExchanger.cs
+++ TestConcurrencyUtilities/TestExchanger.cs
@@ -7,46 +7,46 @@ using System.Threading;
 
 namespace TestConcurrencyUtilities
 {
-	public class TestRendezvous
+	public class TestExchanger
 	{
 		static int _sleepTime;
-		static Rendezvous _rendezvous;
+		static Exchanger _Exchanger;
 
-		static void AttendRendezvousAs(char agentCodename, int sleepTimeBeforeRendezvous = 0) {
-			TestSupport.SleepThread(sleepTimeBeforeRendezvous);
-			TestSupport.DebugThread("is arriving at the rendezvous");
+		static void AttendExchangeAs(char agentCodename, int sleepTimeBeforeExchange = 0) {
+			TestSupport.SleepThread(sleepTimeBeforeExchange);
+			TestSupport.DebugThread("is arriving at the exchange");
 			switch (agentCodename) {
 			case 'A':
-				_rendezvous.AArrive();
+				_Exchanger.AArrive();
 				break;
 			case 'B':
-				_rendezvous.BArrive();
+				_Exchanger.BArrive();
 				break;
 			}
-			TestSupport.DebugThread("has left the rendezvous");
+			TestSupport.DebugThread("has left the exchange");
 		}
 
-		static void AttendRendezvousAsA() {
-			AttendRendezvousAs('A');
+		static void AttendExchangeAsA() {
+			AttendExchangeAs('A');
 		}
 		
-		static void AttendRendezvousAsB() {
-			AttendRendezvousAs('B', _sleepTime);
+		static void AttendExchangeAsB() {
+			AttendExchangeAs('B', _sleepTime);
 		}
 
 		public static void Run(int sleepTime) {
 			_sleepTime = sleepTime;
-			_rendezvous = new Rendezvous();
+			_Exchanger = new Exchanger();
 
-			TestSupport.Log(ConsoleColor.Blue, "Rendezvous test\n==============================");
+			TestSupport.Log(ConsoleColor.Blue, "Exchanger test\n==============================");
 
-			TestSupport.Log(ConsoleColor.Blue, "\nTwo secret agent threads will arrive at a rendezvous, waiting " +
-				"for the other to arrive before parting ways.\n");
+			TestSupport.Log(ConsoleColor.Blue, "\nTwo secret agent threads will arrive at an exchange (rendezvous), " +
+				"waiting for the other to arrive before parting ways.\n");
 			TestSupport.SleepThread(_sleepTime);
 
 			List<Thread> threads = new List<Thread>();
-			threads.AddRange( TestSupport.CreateThreads(AttendRendezvousAsA, "Agent A", 1) );
-			threads.AddRange( TestSupport.CreateThreads(AttendRendezvousAsB, "Agent B", 1) );
+			threads.AddRange( TestSupport.CreateThreads(AttendExchangeAsA, "Agent A", 1) );
+			threads.AddRange( TestSupport.CreateThreads(AttendExchangeAsB, "Agent B", 1) );
 			TestSupport.RunThreads(threads);
 		}
 	}
