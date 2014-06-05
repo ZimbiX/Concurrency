0b0ce6c8838838fc5eefbf2f9e5ccb783cd75d61
Brendan Weibrecht <zimbico@gmail.com>
2014-06-05 18:13:20 +1000

    TestSupport: Add (optional) menu

--- TestConcurrencyUtilities/MainClass.cs
+++ TestConcurrencyUtilities/MainClass.cs
@@ -11,16 +11,42 @@ namespace TestConcurrencyUtilities
 
 			int sleepTimeMs = (int)Math.Round(sleepTimeSeconds*1000);
 
-//			TestSemaphore.Run(10, sleepTimeMs);
-//			TestChannel.Run(10, sleepTimeMs);
-//			TestBoundChannel.Run(10, 3, sleepTimeMs);
-//			TestMutex.Run(100000);
-//			TestLatch.Run(4, sleepTimeMs);
-//			TestBarrier.Run(4, sleepTimeMs);
-			TestLightSwitch.Run(5, sleepTimeMs);
-//			TestExchanger.Run(12, sleepTimeMs);
+			Console.WriteLine("Which test do you want to run?" +
+				"\n  1. Semaphore" +
+				"\n  2. Channel" +
+				"\n  3. Bound Channel" +
+				"\n  4. Mutex" +
+				"\n  5. Latch" +
+				"\n  6. Barrier" +
+				"\n  7. Light Switch" +
+				"\n  8. Exchanger" +
+				"\n  Q. Quit");
+			string response = "INVALID_RESPONSE";
+//			response = "1"; // Can preload with an option to choose automatically
+			do {
+				if (response == "INVALID_RESPONSE") {
+					Console.Write("Enter: ");
+					response = Console.ReadLine().ToUpper();
+				}
+				switch (response) {
+				case "1": TestSemaphore.Run(10, sleepTimeMs); 			break;
+				case "2": TestChannel.Run(10, sleepTimeMs); 			break;
+				case "3": TestBoundChannel.Run(10, 3, sleepTimeMs); 	break;
+				case "4": TestMutex.Run(100000); 						break;
+				case "5": TestLatch.Run(4, sleepTimeMs); 				break;
+				case "6": TestBarrier.Run(4, sleepTimeMs); 				break;
+				case "7": TestLightSwitch.Run(5, sleepTimeMs); 			break;
+				case "8": TestExchanger.Run(12, sleepTimeMs); 			break;
+				case "Q": break;
+				default:
+					Console.WriteLine("Invalid response");
+					response = "INVALID_RESPONSE";
+					break;
+				}
+			} while (response == "INVALID_RESPONSE");
 
-			TestSupport.Log(ConsoleColor.DarkGreen, "\nAll threads have finished");
+			if (response != "Q")
+				TestSupport.Log(ConsoleColor.DarkGreen, "\nAll threads have finished");
 		}
 	}
 }
