c0be2d8cb004649068a04468d0595d7bc21dfaa5
Brendan Weibrecht <zimbico@gmail.com>
2014-06-05 14:32:09 +1000

    Exchanger: Fix by re-write; Add explanation; Update completion status

--- ConcurrencyUtilities/Exchanger.cs
+++ ConcurrencyUtilities/Exchanger.cs
@@ -7,10 +7,10 @@ using System.Threading;
 
 namespace ConcurrencyUtilities
 {
-	// TODO: describe
-	// Status: TODO: fix invalid pairing, test complete, TODO: get marked off
+	// Status: complete, test complete, TODO: get marked off
 	/// <summary>
-	/// An exchanger allows two threads to meet and exchange data.
+	/// An exchanger allows two threads to meet and exchange data. If the other thread is not yet present at the
+	/// rendezvous, it is waited upon.
 	/// </summary>
 	public class Exchanger<T>
 	{
@@ -20,7 +20,8 @@ namespace ConcurrencyUtilities
 		T _dataForB;
 		Mutex _accessToNextThreadIsB;
 		bool _nextThreadIsB;
-		Semaphore _numAgentsLimiter;
+		Semaphore _agentPairer;
+		Semaphore _dataHasBeenTaken;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="ConcurrencyUtilities.Exchanger"/> class.
@@ -30,55 +31,59 @@ namespace ConcurrencyUtilities
 			_bArrived = new Semaphore(0);
 			_accessToNextThreadIsB = new Mutex();
 			_nextThreadIsB = false;
-			_numAgentsLimiter = new Semaphore(2);
+			_agentPairer = new Semaphore(2);
+			_dataHasBeenTaken = new Semaphore(0);
 		}
 
 		/// <summary>
-		/// Arrives at the exchanger to exchange data.
+		/// Arrives at the exchanger and exchanges data with another thread. Threads are let through in pairs, and
+		/// assigned an identity A or B so that each one uses its own one of the temporary storage variables.
+		/// B threads are also in charge of letting a new pair od threads into the exchanger once the existing exchange
+		/// is confirmed to be completed. If each thread were to let a new one in when finished, its partner may not
+		/// have acquired the data that was left, which could then be overwritten before it is accessed.
 		/// </summary>
 		/// <returns>The data received from the other thread.</returns>
 		/// <param name="dataToGive">The data to give to the other thread.</param>
 		public T Arrive(T dataToGive) {
-			_numAgentsLimiter.Acquire(); // Doesn't seem to help...
-				T dataReceived;
-				_accessToNextThreadIsB.Acquire();
-				if (!_nextThreadIsB) {
-					// Will run as thread A
-					_nextThreadIsB = !_nextThreadIsB; // Force the next thread to run as thread B
-					_accessToNextThreadIsB.Release(); // Let the next thread arrive
-					dataReceived = ExchangeAsA(dataToGive);
-				} else {
-					// Will run as thread B
-					_nextThreadIsB = !_nextThreadIsB; // Force the next thread to run as thread A
-					_accessToNextThreadIsB.Release(); // Let the next thread arrive
-					dataReceived = ExchangeAsB(dataToGive);
-				}
-			_numAgentsLimiter.Release();
-			return dataReceived;
-		}
+			_agentPairer.Acquire(); // Let only a pair of threads pass at a time
 
-		/// <summary>
-		/// Exchanges the data as thread A.
-		/// </summary>
-		/// <returns>The data received from thread B.</returns>
-		/// <param name="dataToGive">The data to give to thread B.</param>
-		T ExchangeAsA(T dataToGive) {
-			_dataForB = dataToGive;
-			_aArrived.Release(); // Indicate that our data is ready for collection by thread B
-			_bArrived.Acquire(); // Wait for thread B to arrive and surrender its data
-			return _dataForA;
-		}
-		
-		/// <summary>
-		/// Exchanges the data as thread B.
-		/// </summary>
-		/// <returns>The data received from thread A.</returns>
-		/// <param name="dataToGive">The data to give to thread A.</param>
-		T ExchangeAsB(T dataToGive) {
-			_dataForA = dataToGive;
-			_bArrived.Release(); // Indicate that our data is ready for collection by thread A
-			_aArrived.Acquire(); // Wait for thread A to arrive and surrender its data
-			return _dataForB;
+			// Assign identity to each thread
+
+			char identity;
+			_accessToNextThreadIsB.Acquire();
+				identity = _nextThreadIsB ? 'B' : 'A';
+				_nextThreadIsB = !_nextThreadIsB;
+			_accessToNextThreadIsB.Release();
+
+			T dataReceived;
+
+			// Exchange the data
+
+			if (identity == 'A') { 				// Thread A
+				_dataForB = dataToGive;
+				// Rendezvous
+				_aArrived.Release(); // Indicate that our data is ready for collection by thread B
+				_bArrived.Acquire(); // Wait for thread B to arrive and surrender its data
+				dataReceived = _dataForA;
+				_dataHasBeenTaken.Release();
+			} else { 							// Thread B
+				_dataForA = dataToGive;
+				// Rendezvous
+				_bArrived.Release(); // Indicate that our data is ready for collection by thread A
+				_aArrived.Acquire(); // Wait for thread A to arrive and surrender its data
+				dataReceived = _dataForB;
+				_dataHasBeenTaken.Release();
+			}
+
+			// Once the data exchange is complete, let a new pair of threads into the exchanger
+
+			if (identity == 'B') {
+				_dataHasBeenTaken.Acquire();
+				_dataHasBeenTaken.Acquire();
+				_agentPairer.Release(2);
+			}
+
+			return dataReceived;
 		}
 	}
 }
