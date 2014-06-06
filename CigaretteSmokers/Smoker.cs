ZimbiX/Concurrency

commit b37f48ebee6f28ab78c6c01941b29c4af40bddea

Cigarette Smokers: Complete (besides documentation): Add issue demonstration, separating out my 'Resource turnstiling and iteration checking' solution strategy; Add smoker greedy time; Add menu for selecting speed and solution strategy; Add an Iteration Manager class so as not to modify the 'agent' (table) code from the problem specification; Optimise the smoker starting a new iteration (start the new one before smoking)

CigaretteSmokers/Smoker.cs
@@ -1,6 +1,7 @@
 using System;
 using ThreadSupport = TestConcurrencyUtilities.TestSupport;
 using System.Collections.Generic;
+using ConcurrencyUtilities;
 
 namespace CigaretteSmokers
 {
@@ -9,36 +10,60 @@ namespace CigaretteSmokers
 		public string Name { get; private set; }
 		Table _table;
 		List<SmokerResource> _resourcesNeeded;
+		int _greedyTime;
+		IterationManager _iterationManager;
 
-		public Smoker(string name, Table table, List<SmokerResource> resourcesNeeded) {
+		public Smoker(string name, List<SmokerResource> resourcesNeeded, Table table, 
+		              IterationManager iterationManager, int greedyTime = 0) {
 			Name = name;
 			_table = table;
 			_resourcesNeeded = resourcesNeeded;
+			_iterationManager = iterationManager;
+			_greedyTime = greedyTime;
 		}
 
-		public void Run() {
+		public void RunIssueDemonstration() {
 			while (true) {
-				Object iterIDAtStart = _table.IterationID; // TODO: lock
+				foreach (SmokerResource r in _resourcesNeeded) {
+					ThreadSupport.DebugThread("{yellow}Acquiring: " + r.Name + "...");
+					r.Take();
+					ThreadSupport.DebugThread("{green}Acquired: " + r.Name);
+				}
+				ThreadSupport.DebugThread("{green}Smoking");
+				ThreadSupport.SleepThread(_greedyTime);
+				_table.ReadyToStartNewIteration();
+			}
+		}
+
+		public void RunSolutionByResourceTurnstilingAndIterationChecking() {
+			while (true) {
+				_iterationManager.AccessToIterationID.Acquire();
+					Object iterIDAtStart = _iterationManager.IterationID;
+				_iterationManager.AccessToIterationID.Release();
 				_resourcesNeeded[0].Take();
 				_resourcesNeeded[0].Return();
 				// Check that we're still on the same iteration. If not, start again
-				if (_table.IterationID == iterIDAtStart) { // No need to acquire permission; not critical
+				if (_iterationManager.IterationID == iterIDAtStart) { // No need to acquire permission; not critical
 					_resourcesNeeded[1].Take();
 					_resourcesNeeded[1].Return();
 					// Check that we're still on the same iteration. If not, start again
-					_table.AccessToIterationID.Acquire(); // Need to acquire permission for final verification
-						if (_table.IterationID == iterIDAtStart) { // TODO: lock
+					bool successfullyAcquiredResourcesNeeded = false;
+					_iterationManager.AccessToIterationID.Acquire(); // Need to acquire permission for final verification
+						if (_iterationManager.IterationID == iterIDAtStart) {
+							successfullyAcquiredResourcesNeeded = true;
 							foreach (SmokerResource r in _resourcesNeeded) {
 								r.Take();
 								ThreadSupport.DebugThread("{black}Acquired: " + r.Name);
 							}
-							ThreadSupport.DebugThread("{green}Smoking");
-							ThreadSupport.SleepThread(1000);
-						_table.ReadyToStartNewIteration(); /* It's ok for this to be inside the AccessToIterationID
-							permisson; the table will just wait after acquiring its permission to start a
-							new iteration */
 						}
-					_table.AccessToIterationID.Release();
+					_iterationManager.AccessToIterationID.Release();
+					if (successfullyAcquiredResourcesNeeded) {
+						ThreadSupport.DebugThread("{green}Smoking");
+						ThreadSupport.SleepThread(_greedyTime);
+						_iterationManager.NewIteration();
+						_table.ReadyToStartNewIteration();
+						// You'd do any actual work now
+					}
 				}
 			}
 		}

CigaretteSmokers/CigaretteSmokers.csproj
@@ -37,6 +37,7 @@
     <Compile Include="Smoker.cs" />
     <Compile Include="Table.cs" />
     <Compile Include="SmokerResource.cs" />
+    <Compile Include="IterationManager.cs" />
   </ItemGroup>
   <Import Project="$(MSBuildBinPath)\Microsoft.CSharp.targets" />
   <ItemGroup>
@@ -47,6 +48,10 @@
     <ProjectReference Include="..\TestConcurrencyUtilities\TestConcurrencyUtilities.csproj">
       <Project>{855A46C7-4AF4-426A-BAA3-F651CC54363F}</Project>
       <Name>TestConcurrencyUtilities</Name>
+    </ProjectReference>
+    <ProjectReference Include="..\AnsiColor\AnsiColor.csproj">
+      <Project>{9ABB8BD3-A26A-4E60-A28A-9AF0B98C8CAF}</Project>
+      <Name>AnsiColor</Name>
     </ProjectReference>
   </ItemGroup>
 </Project>

CigaretteSmokers/IterationManager.cs
@@ -1,0 +1,25 @@
+using System;
+using ConcurrencyUtilities;
+
+namespace CigaretteSmokers
+{
+	public class IterationManager
+	{
+		public Object IterationID { get; private set; }
+		public Mutex AccessToIterationID { get; private set; }
+		Table _table;
+
+		public IterationManager(Table table) {
+			_table = table;
+			IterationID = new Object();
+			AccessToIterationID = new Mutex();
+		}
+
+		public void NewIteration() {
+			AccessToIterationID.Acquire();
+				IterationID = new Object();
+			AccessToIterationID.Release();
+			_table.ReadyToStartNewIteration();
+		}
+	}
+}

CigaretteSmokers/MainClass.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Threading;
+using ConcurrencyUtilities;
+using Mutex = ConcurrencyUtilities.Mutex;
+using Colorizer = AnsiColor.AnsiColor;
 using ThreadSupport = TestConcurrencyUtilities.TestSupport;
 
 namespace CigaretteSmokers
@@ -8,6 +11,47 @@ namespace CigaretteSmokers
 	class MainClass
 	{
 		public static void Main(string[] args) {
+			Console.WriteLine(Colorizer.Colorize("{blue}Cigarette Smokers problem" +
+			                                     "\n===============================\n"));
+
+			// Determine smoker greediness delay time
+			
+			int smokerGreedyTime;
+			Console.Write("Do you want to have lightning-fast smokers?\nEnter [y/N]:");
+			string response = Console.ReadLine();
+//			response = "Y"; // Can preload with an option to use regardless
+			if (response.ToUpper() == "Y")
+				smokerGreedyTime = 0;
+			else
+				smokerGreedyTime = 2000;
+
+			// Determine which strategy to use
+
+			string strategy = "";
+			// I'd like to implement the LBoS solution strategy for this problem if I have time
+			Console.WriteLine("\nWhich strategy do you want to use?" +
+			                  "\n  0. None (issue demonstration)" +
+			                  "\n  1. Resource turnstiling and iteration checking" +
+			                  "\n  Q. Quit");
+			response = "INVALID_RESPONSE";
+//			response = "1"; // Can preload with an option to choose automatically
+			do {
+				if (response == "INVALID_RESPONSE") {
+					Console.Write("Enter: ");
+					response = Console.ReadLine().ToUpper();
+				}
+				switch (response) {
+				case "0": strategy = "NONE";						 				break;
+				case "1": strategy = "RESOURCE_TURNSTILING_AND_ITERATION_CHECKING";	break;
+				case "Q": return; // Exit
+				default:
+					Console.WriteLine("Invalid response");
+					response = "INVALID_RESPONSE";
+					break;
+				}
+			} while (response == "INVALID_RESPONSE");
+			Console.WriteLine();
+
 			// Create resources
 
 			List<SmokerResource> resources = new List<SmokerResource>();
@@ -18,6 +62,10 @@ namespace CigaretteSmokers
 
 			Table table = new Table(resources);
 
+			// Set up iteration tracking -- for resource turnstiling and iteration checking solution strategy
+
+			IterationManager iterationManager = new IterationManager(table);
+
 			// Create smokers
 
 			List<Smoker> smokers = new List<Smoker>();
@@ -25,7 +73,7 @@ namespace CigaretteSmokers
 				List<SmokerResource> resourcesNeededBySmoker = new List<SmokerResource>(resources);
 				string smokerName = "S+" + resources[i].Name;
 				resourcesNeededBySmoker.RemoveAt(i); // Remove the resource that they don't require any more of
-				smokers.Add(new Smoker(smokerName, table, resourcesNeededBySmoker));
+				smokers.Add(new Smoker(smokerName, resourcesNeededBySmoker, table, iterationManager, smokerGreedyTime));
 			}
 
 			// Create threads
@@ -34,7 +82,12 @@ namespace CigaretteSmokers
 			List<Thread> threads = new List<Thread>();
 			threads.AddRange(ThreadSupport.CreateThreads(table.Run, "Table", 1, -1, columnWidth, 1));
 			for (int i = 0; i < 3; i++) {
-				threads.AddRange(ThreadSupport.CreateThreads(smokers[i].Run, smokers[i].Name, 1, -1, columnWidth, 2+i));
+				ThreadStart threadMethod;
+				if (strategy == "RESOURCE_TURNSTILING_AND_ITERATION_CHECKING")
+					threadMethod = smokers[i].RunSolutionByResourceTurnstilingAndIterationChecking;
+				else
+					threadMethod = smokers[i].RunIssueDemonstration;
+				threads.AddRange(ThreadSupport.CreateThreads(threadMethod, smokers[i].Name, 1, -1, columnWidth, 2+i));
 			}
 			ThreadSupport.EndColumnHeader(4, columnWidth);
 			ThreadSupport.RunThreads(threads);

CigaretteSmokers/Table.cs
@@ -9,13 +9,10 @@ namespace CigaretteSmokers
 	{
 		List<SmokerResource> _resources;
 
-		public Object IterationID { get; private set; }
-		public Mutex AccessToIterationID { get; private set; }
 		Semaphore _startIteration;
 		Random _rnd;
 
 		public Table(List<SmokerResource> resources) {
-			AccessToIterationID = new Mutex();
 			_startIteration = new Semaphore(0);
 			_resources = resources;
 			_rnd = new Random();
@@ -28,12 +25,9 @@ namespace CigaretteSmokers
 				// Wait for a new iteration to begin before continuing (signalled by constructor / successful smoker)
 				_startIteration.Acquire();
 
-				// Start a new iteration
-				
-				AccessToIterationID.Acquire();
-					IterationID = new Object();
-					RestockTwoResources();
-				AccessToIterationID.Release();
+				// Start a new iteration. If using the iteration checking method, the IterationID has already been
+				// updated by the successful smoker -- avoiding modifying the 'agent' (table) from the original problem
+				RestockTwoResources();
 			}
 		}
 

commit 75ddf4dbcb383c3c357d83eb97eecae484bd7cb6

Cigarette Smokers: Fix bug introduced in b37f48e due to calling '_table.ReadyToStartNewIteration()' an extra time (already taken care of in '_iterationManager.NewIteration()' on the previous line)

CigaretteSmokers/Smoker.cs
@@ -95,7 +95,6 @@ namespace CigaretteSmokers
 						ThreadSupport.DebugThread("{green}Smoking");
 						ThreadSupport.SleepThread(_greedyTime);
 						_iterationManager.NewIteration(); // Update the iteration ID for a new iteration
-						_table.ReadyToStartNewIteration(); // Allow the 'agent' table to restock more resources
 						// You'd do any actual work now
 					}
 				}