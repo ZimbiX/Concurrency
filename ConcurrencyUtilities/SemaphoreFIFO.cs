d68f38c63be91c51698ba6ac8a9198f10eb2882f
Brendan Weibrecht <zimbico@gmail.com>
2014-06-09 02:44:30 +1000

    FIFO Semaphore: Update completion status

--- ConcurrencyUtilities/SemaphoreFIFO.cs
+++ ConcurrencyUtilities/SemaphoreFIFO.cs
@@ -9,7 +9,7 @@ namespace ConcurrencyUtilities
 	/// A FIFO Semaphore is a semaphore where delayed requests for tokens are granted in the order that they were made.
 	/// 
 	/// </summary>
-	// Status: TODO: check if complete, TODO: check if test complete, TODO: get marked off
+	// Status: complete, test complete, TODO: get marked off
 	public class SemaphoreFIFO
 	{
 		int _numTokens;
