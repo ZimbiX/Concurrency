7eba82c7c18352388ee6edeadb3c06be8bc2f99f
Brendan Weibrecht <zimbico@gmail.com>
2014-06-09 02:49:53 +1000

    Reader-Writer Lock: Complete code for utility (from LBOS 'No-starve readers-writers solution') and test; Add more XML documentation for the class; Convert the method documentation to XML; Remove (unnecessary) internal debugging; Split test into multiple rounds with different periods between acquiring; Update completion status;

--- ConcurrencyUtilities/ReaderWriter.cs
+++ ConcurrencyUtilities/ReaderWriter.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading;
+using Colorizer = AnsiColor.AnsiColor;
 
 namespace ConcurrencyUtilities
 {
@@ -7,49 +8,74 @@ namespace ConcurrencyUtilities
 	/// A reader-writer can be used to manage permisson between threads that need read access or write access
 	/// to something. Multiple threads can read something simultaneously with no problem, but when a thread
 	/// needs to write to it, that thread must have sole access.
-	/// A reader-write allows simultaneously either:
+	/// A reader-writer allows simultaneously either:
 	/// - One writer; or
 	/// - Any number of readers
-	/// This version of ReaderWriter gives readers and writers fairer treatment (than a writer having to wait for
-	/// there to be no readers before getting its turn).
+	/// This version of ReaderWriter gives readers and writers fairer treatment (than a writer being blocked out by
+	/// having to wait for there to be no readers, while there actually isn't a gap where there aren't any readers).
+	/// When a writer wants to enter the room, new readers (and wrtiers) are blocked from doing so. When the room
+	/// becomes empty, the writer progresses into the room, does its business, and leaves, opening up access to new
+	/// readers and writers again.
+	/// Readers use a light switch to access the room. The '_roomEmpty' core permission is managed by this light switch.
 	/// </summary>
-	// Status: TODO: finish, TODO: update test, TODO: get marked off
+	// Status: complete, test complete, TODO: get marked off
 	public class ReaderWriter
 	{
-		Mutex _permisson;
-		LightSwitch _lightSwitch;
+		Mutex _roomEmpty;
+		LightSwitch _readSwitch;
+		Mutex _turnstile;
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ConcurrencyUtilities.ReaderWriter"/> class.
+		/// </summary>
 		public ReaderWriter() {
-			_permisson = new Mutex();
-			_lightSwitch = new LightSwitch(_permisson);
+			_roomEmpty = new Mutex(); /* Whether the room is empty -- effectively the core permission managed 
+			                           by the reader-writer lock */
+			_readSwitch = new LightSwitch(_roomEmpty); /* Keeps track of how many readers are in the room (and so 
+			                                            whether the room is empty) */
+			_turnstile = new Mutex(); // A turnstile that controls whether new readers or writers can enter the room
 		}
 
-		//		private void DebugThread(string message) {
-		//			string threadNameWithoutPrefix = Thread.CurrentThread.Name.Replace("%%%%","");
-		//			string threadColumnOffset = threadNameWithoutPrefix.Replace(threadNameWithoutPrefix.TrimStart(' '), "");
-		//			Console.WriteLine(threadColumnOffset + message);
-		//		}
-
-		// Acquire read permisson - join the reading group
+		/// <summary>
+		/// Acquire read permisson -- join the reading group
+		/// </summary>
 		public void ReaderAcquire() {
-			// DebugThread("Wait:...");
-			// ....
-		}
+			// Wait for there to be no writers in the room
+			_turnstile.Acquire();
+			_turnstile.Release();
 
-		// Acquire write permisson - ensure sole access
-		public void WriterAcquire() {
-			// ....
-			_permisson.Acquire();
-		}
+			_readSwitch.Acquire(); // Enter the room using the readers' light switch (which manages _roomEmpty)
 
-		// Release read permisson - leave the reading group
+			// Critical section begins
+		}
+		
+		/// <summary>
+		/// Release read permisson -- leave the reading group
+		/// </summary>
 		public void ReaderRelease() {
-			_lightSwitch.Release();
+			// Critical section ends
+
+			_readSwitch.Release(); // Leave the room using the readers' light switch (which manages _roomEmpty)
 		}
 
-		// Release write permisson - make permission free for all to take
+		/// <summary>
+		/// Acquire write permisson -- ensure sole access
+		/// </summary>
+		public void WriterAcquire() {
+			_turnstile.Acquire(); // Prevent new readers or writers from entering the room
+			_roomEmpty.Acquire(); // Wait for there to be no readers or writers in the room
+			
+			// Critical section begins
+		}
+		
+		/// <summary>
+		/// Release write permisson -- make permission free for all to take
+		/// </summary>
 		public void WriterRelease() {
-			_permisson.Release();
+			// Critical section ends
+
+			_turnstile.Release(); // Allow new readers or writers to enter the room
+			_roomEmpty.Release(); // Signal that the room is empty
 		}
 	}
 }
--- TestConcurrencyUtilities/TestReaderWriter.cs
+++ TestConcurrencyUtilities/TestReaderWriter.cs
@@ -8,14 +8,14 @@ namespace TestConcurrencyUtilities
 	public static class TestReaderWriter
 	{
 		static ReaderWriter _readerWriter;
-		static int _sleepTime;
+		static int _sleepTimeLocked;
 
 		public static void Reader() {
 			TestSupport.DebugThread("{yellow}RAcq");
 			_readerWriter.ReaderAcquire();
 			TestSupport.DebugThread("{green}RAcq");
 
-			TestSupport.SleepThread(_sleepTime);
+			TestSupport.SleepThread(_sleepTimeLocked);
 
 			TestSupport.DebugThread("{yellow}RRel");
 			_readerWriter.ReaderRelease();
@@ -27,38 +27,54 @@ namespace TestConcurrencyUtilities
 			_readerWriter.WriterAcquire();
 			TestSupport.DebugThread("{red}WAcq");
 
-			TestSupport.SleepThread(_sleepTime);
+			TestSupport.SleepThread(_sleepTimeLocked);
 
 			TestSupport.DebugThread("{yellow}WRel");
 			_readerWriter.WriterRelease();
 			TestSupport.DebugThread("{cyan}WRel");
 		}
 
-		public static void Run(int sleepTime) {
+		public static void Run(int sleepTimeLocked = 2000, int sleepTimeBetweenNewRequests = 300) {
+			_sleepTimeLocked = sleepTimeLocked;
+			int sleepTimeBetweenRounds = 3000;
+
+			TestSupport.Log(ConsoleColor.Blue, "Reader-Writer Lock test\n==============================");
+
+			RunTestRound(sleepTimeLocked);
+			TestSupport.SleepThread(sleepTimeBetweenRounds);
+			RunTestRound(sleepTimeBetweenNewRequests);
+			TestSupport.SleepThread(sleepTimeBetweenRounds);
+			RunTestRound(0);
+		}
+
+		static void RunTestRound(int sleepTimeBetweenNewRequests) {
 			_readerWriter = new ReaderWriter();
-			_sleepTime = sleepTime;
-			_sleepTime = 2000; // TODO: remove
 
+			TestSupport.Log(ConsoleColor.Blue, "\nTest with access duration of " +
+			                TestSupport.StringFromMilliseconds(_sleepTimeLocked) + ", and " +
+			                TestSupport.StringFromMilliseconds(sleepTimeBetweenNewRequests) + " between new requests" +
+			                "\n------------------------------\n");
+			
 			// Create the threads:
-
-			const int columnWidth = 8;
+			
+			const int columnWidth = 10;
 			List<Thread> threads = new List<Thread>();
+			threads.AddRange(TestSupport.CreateThreads(Writer, "W", 1, 0, columnWidth, threads.Count + 1));
+			threads.AddRange(TestSupport.CreateThreads(Reader, "R", 4, 0, columnWidth, threads.Count + 1));
 			threads.AddRange(TestSupport.CreateThreads(Writer, "W", 2, 0, columnWidth, threads.Count + 1));
 			threads.AddRange(TestSupport.CreateThreads(Reader, "R", 2, 0, columnWidth, threads.Count + 1));
-			threads.AddRange(TestSupport.CreateThreads(Writer, "W", 2, 0, columnWidth, threads.Count + 1));
-			threads.AddRange(TestSupport.CreateThreads(Reader, "R", 2, 0, columnWidth, threads.Count + 1));
-//			threads.AddRange(TestSupport.CreateThreads(Writer, "W", 1, 0, columnWidth, threads.Count + 1));
+			threads.AddRange(TestSupport.CreateThreads(Writer, "W", 1, 0, columnWidth, threads.Count + 1));
 			TestSupport.EndColumnHeader(threads.Count, columnWidth);
-
+			
 			// Start the threads:
-
+			
 			foreach (Thread t in threads) {
-				TestSupport.SleepThread(100, false);
+				TestSupport.SleepThread(sleepTimeBetweenNewRequests, false);
 				t.Start();
 			}
-
+			
 			// Wait for all the threads to finish:
-
+			
 			TestSupport.JoinThreads(threads);
 		}
 	}
